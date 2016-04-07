771fc45239cfecae56410ef6bc2d63fd83788acc
Michael Smith <michael@spinda.net>
2016-04-07 01:29:52 -0700

    Cairo ARGB32 output is pre-multiplied

--- lib/util.c
+++ lib/util.c
@@ -9,12 +9,11 @@ void copyCairoToWxWidgets ( float r, float g, float b
   unsigned char * cairoEnd = cairo + size;
 
   while (cairo < cairoEnd) {
-    float a1 = (float) cairo[3] / 255;
-    float a2 = 1 - a1;
+    float a = 1 - (float) cairo[3] / 255;
 
-    wx[0] = (unsigned char) ((float) cairo[2] * a1 + r * a2);
-    wx[1] = (unsigned char) ((float) cairo[1] * a1 + g * a2);
-    wx[2] = (unsigned char) ((float) cairo[0] * a1 + b * a2);
+    wx[0] = (unsigned char) ((float) cairo[2] + r * a);
+    wx[1] = (unsigned char) ((float) cairo[1] + g * a);
+    wx[2] = (unsigned char) ((float) cairo[0] + b * a);
 
     cairo += 4;
     wx += 3;
